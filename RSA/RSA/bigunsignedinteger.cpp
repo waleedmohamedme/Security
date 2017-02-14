9ea2109e080d23a61d947e63dbe9eedee76884a1
Waleed Mohamed Mohamed Ali <1201671@eng.asu.edu.eg>
2017-02-14 07:59:14 +0200

    fix division :3

--- RSA/RSA/bigunsignedinteger.cpp
+++ RSA/RSA/bigunsignedinteger.cpp
@@ -7,49 +7,49 @@ using namespace std;
 
 class bigint {
 private:
-	
+
 public:
 
 	string integer = "";
 	deque<unsigned long long > number;
 	bool positive = true;
-	int len = 0;
+	
 	bool dirty = false;
 	bigint() {
 		string integer = "";
 		bool positive = true;
-		int len = 0;
 		
+
 	}
-	bigint(bigint a,int start,int unit=0) {
+	bigint(bigint a, int start, int unit = 0) {
 		string integer = a.integer;
 		bool positive = a.positive;
 		if (unit == 0) {
-			unit = a.number.size()-start;
+			unit = a.number.size() - start;
 		}
-		int len = unit;
-		for (unsigned int i = start; i < start+unit;i++) {
+		
+		for (unsigned int i = start; i < start + unit; i++) {
 			number.push_back(a.number[i]);
 		}
 	}
 	bigint(string value) {
 		integer = value;
 		bool positive = true;
-		len = value.length();
-		int x = 7;
 		
-	while(value.length()>0) {
+		int x = 7;
+
+		while (value.length()>0) {
 			if (value.length() <= 7) {
 				number.push_back(stoull(value));
-	
+
 				break;
 			}
-			number.push_back(stoull(value.substr(value.length()-7, 7)));
-			value = value.substr(0,value.length()-7 );
-			
+			number.push_back(stoull(value.substr(value.length() - 7, 7)));
+			value = value.substr(0, value.length() - 7);
+
 		}
-		
-	
+
+
 	}
 	bigint(long long value) {
 		if (value < 0) {
@@ -80,10 +80,10 @@ public:
 		else {
 			number.push_back((value));
 		}
-	
+
 	}
 	bigint(int value) {
-		
+
 		if (value < 0) {
 			positive = false;
 			value = value*(-1);
@@ -98,12 +98,13 @@ public:
 			number.push_back((value));
 		}
 	}
+	
 	bool ispositive(bigint x) {
 		return positive;
 	}
 	string getinstring() {
 		if (!dirty) {
-		//	return integer;
+			//	return integer;
 		}
 
 		integer = "";
@@ -111,28 +112,23 @@ public:
 		string temp_string;
 		for (unsigned long long n : number) {
 			temp_string = to_string(n);
-			
+
 			if (temp_string.length() < 7) {
 				int l = (7 - temp_string.length());
-				
-				temp_string.insert(0, zeros,0,l );
+
+				temp_string.insert(0, zeros, 0, l);
 			}
-			
+
 			integer.insert(0, temp_string);
 			//cout << temp_string << endl << integer<<endl;
 		}
-		integer = to_string(number[number.size()-1]) + integer.substr(7);
+		integer = to_string(number[number.size() - 1]) + integer.substr(7);
 		dirty = false;
 		//cout << integer << endl;
 		return integer;
 	}
-	void print() {
-
-		cout << getinstring() << endl;
-	}
-	int length() {
-		return getinstring().length();
-	}
+	void print() {cout << getinstring() << endl;}
+	int length() {return getinstring().length();}
 	int toint(char x) {
 		string temp = "0";
 		temp += x;
@@ -225,41 +221,40 @@ public:
 			sum.insert(0, to_string(carry));
 		return sum;
 	}
-	unsigned long long getnum (){
+	unsigned long long getnum() {
 		return stoull(integer);
 	}
-	string getvalue() { return integer; }
 	bigint add(bigint b, bool print = false) {
 		bigint sum;
 		sum.dirty = true;
 		//unsigned long long po = 100000000000000000;
 		unsigned long long po = 10000000;
-		int		tt =  b.number.size();
+		int		tt = b.number.size();
 		if (tt > number.size()) tt = number.size();
 		int i = 0;
 		int carry = 0;
-		for (; i < tt;i++) {
+		for (; i < tt; i++) {
 			unsigned long long m = number[i] + b.number[i] + carry;
 			sum.number.push_back(m%po);
-			carry= (m/po) ? 1 : 0;
+			carry = (m / po) ? 1 : 0;
 		}
 		if (number.size() > b.number.size()) {
 			int diff = number.size() - b.number.size();
 			for (; i < number.size(); i++) {
-				sum.number.push_back(number[i]+carry);
-				unsigned long long m = number[i] +  carry;
-				carry= (m / po) ? 1 : 0;
+				sum.number.push_back(number[i] + carry);
+				unsigned long long m = number[i] + carry;
+				carry = (m / po) ? 1 : 0;
 			}
 		}
 		else if (number.size() < b.number.size()) {
 			int diff = b.number.size() - number.size();
 			for (; i < b.number.size(); i++) {
-				sum.number.push_back(b.number[i]+carry);
+				sum.number.push_back(b.number[i] + carry);
 				unsigned long long m = b.number[i] + carry;
 				carry = (m / po) ? 1 : 0;
 			}
 		}
-		
+
 		return sum;
 	}
 	bigint sub(bigint b, bool print = false) {
@@ -267,7 +262,7 @@ public:
 		final.dirty = true;
 		int finalzero = 0;
 		if (number.size() == 1 && b.number.size() == 1) {
-			if (number[0]>=b.number[0]) {
+			if (number[0] >= b.number[0]) {
 
 				final.number.push_back((number[0] - b.number[0]));
 				return final;
@@ -278,11 +273,11 @@ public:
 				final.number.push_back((b.number[0] - number[0]));
 				return final;
 			}
-			
+
 		}
 
 		int	tt = b.number.size();
-		int bb = b.number.size();
+		int bb = number.size();
 		if (tt > number.size()) tt = number.size();
 		int i = 0;
 		for (int borrow = 0; i < tt; i++) {
@@ -291,13 +286,13 @@ public:
 			long long temp = temp1 - temp2;
 			if (temp<0) {
 				//need carry 
-				for (int j = i + 1; j < bb ; j++) {
+				for (int j = i + 1; j < bb; j++) {
 					if (number[j]>0) {
-						number[j]= (number[j] - 1);
+						number[j] = (number[j] - 1);
 						break;
 					}
 					else if (number[j] == 0) {
-							//number[j]= 99999999999999999;
+						//number[j]= 99999999999999999;
 						number[j] = 9999999;
 					}
 
@@ -312,7 +307,7 @@ public:
 
 			}
 			else {
- 				final.number.push_back((temp1)-(temp2));
+				final.number.push_back((temp1)-(temp2));
 			}
 
 
@@ -331,12 +326,21 @@ public:
 
 	}
 	bigint remove(bigint a) {
-		
-		if ( a.number[a.number.size()-1] != 0)
+
+		if (a.number[a.number.size() - 1] != 0)
 			return a;
-		while (a.number[a.number.size()-1] == 0) {
+		while (a.number[a.number.size() - 1] == 0) {
 
 			a.number.pop_back();
+		}
+		return a;
+	}
+	string remove(string a) {
+		if (a[0] != '0')
+			return a;
+		while (a[0] == '0') {
+
+			a = a.substr(1);
 		}
 		return a;
 	}
@@ -344,10 +348,10 @@ public:
 
 		if (left) {
 			for (int i = 0; i < units; i++)	number.push_back(0);
-			
+
 		}
 		else {
-			for (int i = 0; i < units; i++)	number.insert(number.begin(),0);
+			for (int i = 0; i < units; i++)	number.insert(number.begin(), 0);
 
 		}
 
@@ -369,6 +373,23 @@ public:
 		}
 
 	}
+	string shift_div(string input, int units, bool left = true) {
+
+		if (left) {
+			for (int i = 0; i < units; i++) {
+				input.append("0");
+			}
+			return input;
+		}
+		else {
+			for (int i = 0; i < units; i++) {
+				input.insert(0, "0");
+			}
+			return input;
+
+		}
+
+	}
 	string fit(string a, int size) {
 		int amount = size - a.length();
 		if (amount <= 0) {
@@ -377,11 +398,11 @@ public:
 		return shift(a, amount, false);
 
 	}
-	
+
 	void fit(int size) {
 		int k = number.size() - size;
 		if (k < 0) {
-			for (int i = 0; i <size - number.size();i++)	number.push_back(0);
+			for (int i = 0; i <size - number.size(); i++)	number.push_back(0);
 		}
 
 	}
@@ -398,7 +419,7 @@ public:
 			return 0;
 		}
 		else {
-			for (unsigned int i = 0; i < a.length() - 1; i++) {
+			for (unsigned int i = 0; i < a.length() ; i++) {
 				if (a[i] > b[i]) {
 					if (max) { return 0; }
 					return 1;
@@ -466,61 +487,15 @@ public:
 
 		return a.add(b);
 	}
-	string remove(string a) {
-		if (a[0] != '0')
-			return a;
-		while (a[0] == '0') {
-
-			a = a.substr(1);
-		}
-		return a;
-	}
-
-	string multiply_1(unsigned long long aa, unsigned long long bb) {
-		string product;
-		string x = to_string(aa);
-		string y = to_string(bb);
-		if (x.length() + y.length() <= 18) return to_string((stoull(x)*stoull(y)));
-		int n1 = x.length();
-		int n2 = y.length();
-		x = fit(x, n2);
-		y = fit(y, n1);
-		int m = x.length();
-		int n = m / 2;
-		int sm = m - n;
-		string a = x.substr(0, n);
-		string b = x.substr(n);
-		string c = y.substr(0, n);
-		string d = y.substr(n);
-
-		string ac = multiply_1(stoull(a), stoull(c));
-		string bd = multiply_1(stoull(b), stoull(d));
-		string abcd = multiply_1(stoull(add_string(a, b)), stoull(add_string(c, d)));
-
-
-		abcd = sub_string(abcd, ac);
-		abcd = sub_string(abcd, bd);
-
-		ac = shift(ac, 2 * (sm));
-		abcd = shift(abcd, sm);
-
-		product = add_string(ac, abcd);
-		product = add_string(product, bd);
-
-		return remove(product);
-
-	}
+	
 	bigint multiply(bigint bb, bool print = false) {
 		if (number.size() + bb.number.size() <= 2) {
 			if (to_string(number[0]).length() + to_string(bb.number[0]).length() <= 15) {
 				return bigint(number[0] * bb.number[0]);
 			}
-			else {
-				return bigint(multiply_1(number[0], bb.number[0]));//to be 
-			}
-		
-		} 
-		
+
+		}
+
 		bigint product;
 		int n1 = number.size();
 		int n2 = bb.number.size();
@@ -529,10 +504,10 @@ public:
 		int m = number.size();
 		int n = m / 2;
 		int sm = m - n;
-		bigint b(*this,0, n);
-		bigint a(*this,n);
-		bigint d(bb,0, n);
-		bigint c(bb,n);
+		bigint b(*this, 0, n);
+		bigint a(*this, n);
+		bigint d(bb, 0, n);
+		bigint c(bb, n);
 
 		bigint ac = a.multiply(c);
 		bigint bd = b.multiply(d);
@@ -541,100 +516,130 @@ public:
 		bigint cd = (c.add(d));
 		bigint abcd = ab.multiply(cd);
 
-		abcd =remove(subtruct(abcd, ac));
+		abcd = remove(subtruct(abcd, ac));
 		abcd = remove(subtruct(abcd, bd));
 		ac = shift(ac.getinstring(), 2 * (n));
 		abcd = shift(abcd.getinstring(), n);
 		product = ac.add(abcd);
-		product = remove( product.add(bd));
-		
+		product = remove(product.add(bd));
+
 
 		return product;
 	}
 	bigint linear_mult(bigint bb) {
 
-		 string output="";
-		 int m = 1;
-		 string sum="";
-	for (int i = 0; i <number.size(); i++) {
-        
-		unsigned long long carry=0;
-       
-        for (int j = 0; j <bb.number.size();j++){
-            unsigned long long multi=(number[i]*bb.number[j])+carry;
-            unsigned long long res=multi%10000000;
-            carry=multi/10000000;
-			string re = to_string(res);
-			if (to_string(res).length() < 7) {
-				for (int i = 0; i < 7 - to_string(res).length(); i++) {
-					re.insert(0, "0");
+		string output = "";
+		int m = 1;
+		string sum = "";
+		for (int i = 0; i <number.size(); i++) {
+
+			unsigned long long carry = 0;
+
+			for (int j = 0; j <bb.number.size(); j++) {
+				unsigned long long multi = (number[i] * bb.number[j]) + carry;
+				unsigned long long res = multi % 10000000;
+				carry = multi / 10000000;
+				string re = to_string(res);
+				if (to_string(res).length() < 7) {
+					for (int i = 0; i < 7 - to_string(res).length(); i++) {
+						re.insert(0, "0");
+					}
 				}
+				output.insert(0, re);
 			}
-            output.insert(0,re);
-        }
-        if(carry!=0)
-            output.insert(0,to_string(carry));
-        //cout << "======"<<endl;
-        for(int x=m-1;x>0;x--)
-            output=output+"0000000";
-        //cout <<m <<"    " <<output<<endl;
-        sum=add_string(sum,(output));
-        output="";
-        m++;
-    }
-    return bigint(sum);
+			if (carry != 0)
+				output.insert(0, to_string(carry));
+			//cout << "======"<<endl;
+			for (int x = m - 1; x>0; x--)
+				output = output + "0000000";
+			//cout <<m <<"    " <<output<<endl;
+			sum = add_string(sum, (output));
+			output = "";
+			m++;
+		}
+		return bigint(sum);
 
 	}
-	string divbig(string a, string divisor) {
+	string divbig(string a, string divisor,bool first =true) {
 
-		string final_co = "";
-		string final_reminder = "";
-		string co = "";
-		long long k = 0.0;
-		//step 1
-		int start_limit = divisor.length();
-		int limit = 0;
+		vector<string> m;
 		string d = divisor.substr(0, 1);
-		if (max_min(divisor, a) == 1) {
-			//a is bigger ok
-			unsigned int i = 1;
-			do {
-				k = stoi(a.substr(0, i)) / stoi(d);
-				if (k == 0)
-				{
-					i++;
-				}
-				else {
-					co = to_string(k);
-					break;
-				}
-			} while (i <= a.length());
-
-			string product = remove(bigint(co).linear_mult(divisor).getinstring());
-			product = shift(product, a.length() - product.length());
-			if (max_min(product, a) == 0) {
-				k = k - 1;
-				co = to_string(k);
-				product = remove(bigint(co).linear_mult(divisor).getinstring());
+		int dd = stoi(d);
+		string answer = "";
+		int j = divisor.length();
+		string aa ;
+		string tobesubbed;
+		m.push_back( divisor );
+		m.push_back(bigint("2").linear_mult(bigint(divisor)).getinstring());
+		m.push_back(bigint("4").linear_mult(bigint(divisor)).getinstring());
+		m.push_back(bigint("8").linear_mult(bigint(divisor)).getinstring());
+		string leftover = a;
+		string result="", tempresult = "", zeros = "";
+		while (max_min(leftover, divisor) == 0) {
+			
+			int i = 0;
+			int k = 0;
+			while (i < leftover.length() && k == 0) {
+				k = stoi(leftover.substr(0, ++i)) / dd;
+			}
+			int ll = 0;
+			if (k >= 8) {
+				aa = "8";
+				ll = 3;
+				tobesubbed = m[3];
+			}
+			else if (k >= 4) {
+				aa = "4"; ll = 2;
+				tobesubbed = m[2];
+			}
+			else if (k >= 2) {
+				aa = "2"; ll = 1;
+				tobesubbed = m[1];
+			}
+			else if (k >= 1) {
+				aa = "1"; ll = 0;
+				tobesubbed = m[0];
 			}
 
-			string next = remove(subtruct(bigint(a), bigint(product)).getinstring());
+			zeros = "";
+			if (max_min(tobesubbed , leftover) == 0) {
+				tobesubbed = m[--ll];
+			}
+			for (i = 0; i < (leftover.length() - tobesubbed.length()); i++) zeros.append("0");
+			if (max_min(tobesubbed+zeros, leftover) == 0) {
+				zeros = zeros.substr(0, zeros.length() - 1);
+			}
+			tobesubbed = tobesubbed + zeros;
+			leftover = remove(subtruct(bigint(leftover), bigint(tobesubbed)).getinstring());
+			if (ll==3) {
+				aa = "8";
+				
+			}
+			else if (ll==2) {
+				aa = "4";
+			}
+			else if (ll==1) {
+				aa = "2"; 
+			}
+			else if (ll ==0) {
+				aa = "1"; 
+			}
 
-			return co + divbig(next, divisor);
-
+			tempresult = aa + zeros;
+			result = add(bigint(result), bigint(tempresult)).getinstring();
 		}
-		else if (max_min(divisor, a) == 2) {
-			//same shit 
-			//co = 1;
-			co = "1";
-			return co;
-		}
-		else {
-			co = "";
-			return co;
-		}
+		if (!first)return leftover;
+		return result;
 	}
-
+	bigint divide(bigint bb ,bool div=true) {
+		string a = getinstring();
+		string b = bb.getinstring();
+		string result = divbig(a, b);
+		//sign condtions 
+		//cout <<endl << result << endl;
+		if (div == true) return bigint(result);
+		return (*this).sub(bigint(result).linear_mult(bb));;
+	}
 };
 int max_min(string a, string b, bool max = true) {
 	//if 0 >> a 
@@ -673,26 +678,27 @@ string remove(string a) {
 	return a;
 }
 
-string subtruct(string a , string b ) {
+string subtruct(string a, string b) {
 	int k = max_min(a, b);
-	if(k==2) {
+	if (k == 2) {
 		return "0";
 	}
 	else if (k == 0) {
 		return bigint(a).sub(bigint(b)).getinstring();
-		
+
 	}
 	else {
-		return "-"+ bigint(b).sub(bigint(a)).getinstring();
+		return "-" + bigint(b).sub(bigint(a)).getinstring();
 	}
 }
+/*
 string div_mod(string p, string q, bool div = true) {
 
-	if (div == true) return bigint(p).divbig(p, q);
+	if (div == true) return bigint(p).divide(bigint(q));
 
-	return remove((bigint(p).sub(bigint(q)).linear_mult( bigint(p).divbig(p, q))).getinstring());
+	return remove((bigint(p).sub(bigint(q)).linear_mult(bigint(p).divbig(p, q))).getinstring());
 
-}
+}*/
 void test(string p, string original) {
 	if (original == p) {
 		cout << "Correct Answer \n";
@@ -705,38 +711,42 @@ int main() {
 	string p = "12369571528747655798110188786567180759626910465726920556567298659370399748072366507234899432827475865189642714067836207300153035059472237275816384410077871";
 	string q = "2065420353441994803054315079370635087865508423962173447811880044936318158815802774220405304957787464676771309034463560633713497474362222775683960029689473";
 	string pq = "25548364798832019218170326077010425733930233389897468141147917831084690989884562791601588954296621731652139141347541240725432606132471100644835778517336041031200174441223836394229943651678525471050219216183727749114047330431603023948126844573697946795476319956787513765533596926704755530772983549787878951983";
-bigint y(p);
-	bigint x(q);
-	bigint z(pq);
-	//	p = "666666666666666";
-	
-//q = "111111111111111";
-	//pq= "74074074074073925925925925926";
-//	p = "1234506700891999";
-//	q = "1234506700891999";
-//	pq = "15241556995136506682262216001";
 	string plus = "14434991882189650601164503865937815847492418889689094004379178704306717906888169281455304737785263329866414023102299767933866532533834460051500344439767344";
 	string minus = "10304151175305660995055873707196545671761402041764747108755418614434081589256563733014494127869688400512871405033372646666439537585110014500132424380388398";
+
+
+	//p = "12369571";
+	//q = "2065420";
+	//p = "5953191492";
+	//q = "62182";
+	//  pq= "74074074074073925925925925926";
+	//	p = "1234506700891999";
+	//	q = "1234506700891999";
+	//	pq = "15241556995136506682262216001";
+	bigint x(p);
+	bigint y(q);
+	bigint xy(pq);
 	bigint minus2(minus);
-	/*cout << div_mod("8896208", "2654");
-	
+	//cout << div_mod("8896208", "2654");
+
 	//string plus = "35005132";
-	string co = div_mod(p, q);
-	bigint coo(co);
-	string r = div_mod(p, q, false);
-
-	bigint original =(coo.linear_mult(x) ).add( bigint(r));
+	string co = x.divide(y).getinstring();
+	bigint coo = x.divide(y);
+	bigint r = x.divide(y,false);
+	bigint original =(coo.linear_mult(y)).add((r));
+	cout << coo.getinstring()<<endl<<p<<endl;
 	test(original.getinstring(), p);
-	*/
+	
 
-	cout <<pq <<endl<<endl;
-	cout << y.linear_mult(x).getinstring() <<endl;
+	//x.divide(y);
+	//cout << pq << endl << endl;
+	//cout << x.linear_mult(y).getinstring() << endl;
 	clock_t t = clock();
-	for (int i = 0; i < 1000; i++) y.linear_mult(x);;
+	for (int i = 0; i < 1000; i++) x.linear_mult(y);;
 	t = clock() - t;
 	cout << "time :" << (((float)t) / CLOCKS_PER_SEC) << " seconds" << endl;
 
-	//test(y.add(x).getvalue(),plus);
+	
 	cout << "end....\n";
 	return 0;
 }
